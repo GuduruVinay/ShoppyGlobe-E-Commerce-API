3d38595b035242af7314dba6e95cd9e36fcc8df8
GuduruVinay <guduruvinay3@gmail.com>
2025-12-30 18:48:01 +0530

    Integrated MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import productRoute from "./routes/product.routes.js";
 
 // Express Object
 const app = express();
+
+mongoose.connect("mongodb+srv://guduruvinay3_db_user:taBsur5r3zhwcG08@cluster0.spt52f4.mongodb.net/")
+.then((res) => { console.log("DATABASE IS CONNECTED") })
+.catch((err) => { console.log("ERROR IN CONNECTING DATABASE") })
 
 app.use(express.json());
 
@@ -9,6 +15,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Root Route");
 })
+
+productRoute(app)
 
 // Local Host at PORT
 const PORT = 8080;
