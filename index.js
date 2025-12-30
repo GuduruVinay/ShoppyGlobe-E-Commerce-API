a8843425bd70a484ce13efa07af3528c87e8779f
GuduruVinay <guduruvinay3@gmail.com>
2025-12-30 18:18:43 +0530

    Installed Mongoose

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
+// import shoppyglobeRoute from "./routes/shoppyglobe.routes.js"; 
 
+// Express Object
 const app = express();
 
 app.use(express.json());
 
+// Root Route
 app.get('/', (req, res) => {
     res.send("Root Route");
 })
 
+// shoppyglobeRoute(app);
+
+// Local Host at PORT
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server RUNNING ON PORT: ${PORT}`);
