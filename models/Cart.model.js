ccad42e13f541ba7745a5a4b3adf8a6952d3ce4a
GuduruVinay <guduruvinay3@gmail.com>
2026-01-01 08:29:33 +0530

    Updated Cart Model

--- models/Cart.model.js
+++ models/Cart.model.js
@@ -1,17 +1,26 @@
 import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema({
-    productId: {
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: [true, 'Product ID is required']
+        ref: 'User',
+        required: true
     },
-    quantity: {
-        type: Number,
-        required: [true, 'Quantity is required'],
-        min: [1, 'Quantity must be atleast 1'],
-        default: 1
-    }
+    items: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: [true, 'Product ID is required']
+            },
+            quantity: {
+                type: Number,
+                required: [true, 'Quantity is required'],
+                min: [1, 'Quantity must be atleast 1'],
+                default: 1
+            }
+        }
+    ]
 });
 
 const CartModel = mongoose.model('Cart', cartSchema);
