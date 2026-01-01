d202f5ea9052d18729877fc80fdaaccac9a64330
GuduruVinay <guduruvinay3@gmail.com>
2026-01-01 08:26:15 +0530

    Added User Model

---
+++ models/User.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        trim: true,
+        maxlength: [50, 'Name cannot be more than 50 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: [6, 'Password must be atleast 6 characters']
+    }
+})
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel;
